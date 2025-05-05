c1658d3e2ed62268f3411ee6063709b02679b79e
fashni <fashni@tuta.io>
2025-05-06 02:55:04 +0700

    add support for querying by latitude and longitude

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use std::collections::HashMap;
 use worker::*;
 
+const BASE_URL: &str = "https://api.openweathermap.org/data/2.5/weather";
+
 #[event(fetch)]
 async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
   console_error_panic_hook::set_once();
@@ -18,21 +20,27 @@ async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
 
   let url: Url = req.url().unwrap();
   let params: HashMap<_, _> = url.query_pairs().into_owned().collect();
-  let city: &String = match params.get("city") {
-    Some(c) => c,
-    None => {
+
+  let api_key: String = env.var("OPENWEATHER_API_KEY").unwrap().to_string();
+  let api_url: String = match (params.get("city"), params.get("lat"), params.get("lon")) {
+    (Some(city), None, None) => {
+      format!(
+        "{}?q={}&appid={}",
+        BASE_URL, urlencoding::encode(city), api_key
+      )
+    },
+    (None, Some(lat), Some(lon)) => {
+      format!(
+        "{}?lat={}&lon={}&appid={}",
+        BASE_URL, urlencoding::encode(lat), urlencoding::encode(lon), api_key
+      )
+    },
+    _ => {
       return Response::ok(
-        r#"{"cod": "400", "message": "missing city param"}"#
+        r#"{"cod": "400", "message": "invalid parameters"}"#
       ).map(|resp: Response| set_headers(resp, 400))
     }
   };
-
-  let api_key: String = env.var("OPENWEATHER_API_KEY").unwrap().to_string();
-  let api_url: String = format!(
-    "https://api.openweathermap.org/data/2.5/weather?q={}&appid={}",
-    urlencoding::encode(city),
-    api_key
-  );
 
   let client: Fetch = Fetch::Url(api_url.parse().unwrap());
   let mut res: Response = client.send().await?;
